72b156577e9acb3fe2bae327481abb49d469846c
Johnny Ramirez <ramirezjohnny@icloud.com>
2023-07-10 20:14:25 +0000

    Implement exercise 3

--- src/imdb.ml
+++ src/imdb.ml
@@ -3,8 +3,14 @@ open! Core
 (* [get_credits] should take the contents of an IMDB page for an actor and return a list
    of strings containing that actor's main credits. *)
 let get_credits contents : string list =
-  ignore (contents : string);
-  failwith "TODO"
+  let open Soup in
+  parse contents
+  $$ "a[class]"
+  |> to_list
+  |> List.filter ~f:(fun a -> String.equal (R.attribute "class" a) "ipc-primary-image-list-card__title")
+  |> List.map ~f:(fun a -> texts a |> String.concat ~sep:"" |> String.strip)
+  |> List.sort ~compare: (fun a b -> String.compare a b)
+
 ;;
 
 let print_credits_command =
@@ -21,3 +27,8 @@ let print_credits_command =
 let command =
   Command.group ~summary:"imdb commands" [ "print-credits", print_credits_command ]
 ;;
+
+
+(*
+   
+<a class="ipc-primary-image-list-card__title" role="button" tabindex="0" aria-disabled="false" href="/title/tt6710474/?ref_=nm_knf_t_1">Everything Everywhere All at Once</a>*)
