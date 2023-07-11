c9581f0fbd8b1e607d40fc7772eab67c3e2d6334
Johnny Ramirez <ramirezjohnny@icloud.com>
2023-07-11 21:06:05 +0000

    Implement interstate graph

--- src/interstate.ml
+++ src/interstate.ml
@@ -1,4 +1,54 @@
 open! Core
+module City = String
+
+module Interstate = struct
+  include String
+  let default = ""
+end
+
+
+
+(* We separate out the [Network] module to represent our social network in OCaml types. *)
+module Network = struct
+  (* We can represent our social network graph as a set of connections, where a connection
+     represents a friendship between two people. *)
+  module Connection = struct
+    module T = struct
+      type t = City.t * City.t [@@deriving compare, sexp]
+    end
+
+    (* This funky syntax is necessary to implement sets of [Connection.t]s. This is needed
+       to defined our [Network.t] type later. Using this [Comparable.Make] functor also
+       gives us immutable maps, which might come in handy later. *)
+    include Comparable.Make (T)
+
+    (* let of_string s =
+      match String.split s ~on:',' with
+      | [ x; y ] -> Some (City.of_string x, City.of_string y)
+      | _ -> None
+    ;; *)
+  end
+
+  type t = Connection.Set.t [@@deriving sexp_of]
+
+  let of_file input_file =
+    let connections =
+      In_channel.read_lines (File_path.to_string input_file)
+      |> List.concat_map ~f:(fun s ->
+        String.split s ~on:','
+        |> List.tl
+        |> fun c -> match c with 
+        | Some cities -> (
+          List.mapi cities ~f: (fun i city -> 
+        if i <> (List.length cities) - 1 then (City.of_string city, List.nth_exn cities (i + 1)) else (City.of_string city, City.of_string city))
+        |> List.filter ~f: (fun (city1, city2) -> not(String.equal city1 city2))
+        )
+        | None -> ["", ""]
+      ) in
+      print_s [%message ((Connection.Set.of_list connections): Connection.Set.t)];
+    Connection.Set.of_list connections
+  ;;
+end
 
 let load_command =
   let open Command.Let_syntax in
@@ -15,6 +65,35 @@ let load_command =
         ignore (input_file : File_path.t);
         failwith "TODO"]
 ;;
+
+
+(* In order to visualize the social network, we use the ocamlgraph library to create a
+   [Graph] structure whose vertices are of type [Person.t].
+
+   The ocamlgraph library exposes lots of different ways to construct different types of
+   graphs. Take a look at
+   https://github.com/backtracking/ocamlgraph/blob/master/src/imperative.mli for
+   documentation on other types of graphs exposed by this API. *)
+   module G = Graph.Imperative.Digraph.ConcreteBidirectionalLabeled (City) (Interstate)
+
+   (* We extend our [Graph] structure with the [Dot] API so that we can easily render
+      constructed graphs. Documentation about this API can be found here:
+      https://github.com/backtracking/ocamlgraph/blob/master/src/dot.mli *)
+   module Dot = Graph.Graphviz.Dot (struct
+       include G
+   
+       (* These functions can be changed to tweak the appearance of the generated
+          graph. Check out the ocamlgraph graphviz API
+          (https://github.com/backtracking/ocamlgraph/blob/master/src/graphviz.mli) for
+          examples of what values can be set here. *)
+       let edge_attributes _ = [ `Dir `None ]
+       let default_edge_attributes _ = []
+       let get_subgraph _ = None
+       let vertex_attributes v = [ `Shape `Box; `Label v; `Fillcolor 1000 ]
+       let vertex_name v = v
+       let default_vertex_attributes _ = []
+       let graph_attributes _ = []
+     end)
 
 let visualize_command =
   let open Command.Let_syntax in
@@ -35,8 +114,13 @@ let visualize_command =
           ~doc:"FILE where to write generated graph"
       in
       fun () ->
-        ignore (input_file : File_path.t);
-        ignore (output_file : File_path.t);
+        let network = Network.of_file input_file in
+        let graph = G.create () in
+        Set.iter network ~f:(fun (city1, city2) ->
+          (* [G.add_edge] auomatically adds the endpoints as vertices in the graph if
+             they don't already exist. *)
+          G.add_edge graph city1 city2);
+        Dot.output_graph (Out_channel.create (File_path.to_string output_file)) graph;
         printf !"Done! Wrote dot file to %{File_path}\n%!" output_file]
 ;;
 
